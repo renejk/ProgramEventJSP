de33ebbf1279831b7f515d3a29c39558204b7bae
renejk <renejosecardonaherrera@gmail.com>
2024-11-02 20:35:44 -0500

    se añadieron clases para manejo de excepciones

---
+++ src/java/Infrastructure/Database/ConnectionDbMySql.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Infrastructure.Database;
+
+/**
+ *
+ * @author renejk
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDbMySql {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/events_web";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            throw new SQLException("Driver not found: " + DRIVER);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new SQLException("Connection error: " + ex.getMessage());
+        }
+
+        return conn;
+    }
+
+}
